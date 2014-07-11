0b91d8cbde53877d19dbb85374ebe5bd337e9aa7
Thibault BRONCHAIN <thibault@mc2.io>
2014-07-11 22:11:58 +0800

    improve build

--- images/build.sh
+++ images/build.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-docker build -t visualops/ubuntu_wordpress $1
+if [ "$1" = "" ]; then
+    echo "Choose which image to build:"
+    echo " [1] httpd_base"
+    echo " [2] httpd_wordpress"
+    echo
+    read CHOICE
+
+    if [ "$CHOICE" = "2" ]; then
+        IMAGE="httpd_wordpress"
+    else
+        IMAGE="httpd_base"
+    fi
+else
+    IMAGE="$1"
+fi
+
+docker build -t visualops/ubuntu_wordpress $IMAGE
