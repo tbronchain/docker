17ab5e877ce74da9cc960bbdf4be632c3880eb50
Thibault BRONCHAIN <thibault@mc2.io>
2014-07-12 02:15:09 +0800

    fix build

--- images/build.sh
+++ images/build.sh
@@ -3,14 +3,17 @@
 if [ "$1" = "" ]; then
     echo "Choose which image to build:"
     echo " [1] httpd_base"
-    echo " [2] httpd_wordpress"
+    echo " [2] httpd_hello"
+    echo " [3] httpd_wordpress"
     echo
     read CHOICE
 
-    if [ "$CHOICE" = "2" ]; then
-        IMAGE="httpd_wordpress"
-    else
+    if [ "$CHOICE" = "1" ]; then
         IMAGE="httpd_base"
+    elif [ "$CHOICE" = "2" ]; then
+        IMAGE="httpd_hello"
+    else
+        IMAGE="httpd_wordpress"
     fi
 else
     IMAGE="$1"
