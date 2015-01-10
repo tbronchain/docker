26a9d9b05226d1bfd51d887a44f38a48c2945477
Thibault BRONCHAIN <thibault@mc2.io>
2015-01-10 17:43:21 +0800

    launcher script

---
+++ devenv/ssh.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+USAGE="$0 build|run|start|stop|kill|ssh"
+
+case $1 in
+    build)
+        docker build -t ssh .
+        ;;
+    run)
+        docker run --name=ssh --expose=22 -p 2222:22 ssh &
+        ;;
+    start)
+        docker start ssh
+        ;;
+    stop)
+        docker stop ssh
+        ;;
+    kill)
+        docker kill ssh
+        ;;
+    ssh)
+        ssh -p 2222 root@`boot2docker ip`
+        ;;
+    *)
+        echo -e "Syntax error\nusage: $USAGE"
+        ;;
+esac
+
